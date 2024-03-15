b26ead3cd893c72706d6b4d9a996d6e2461c5bf0
Mohammad Noroozi <mohammadnoroozipk82@gmail.com>
2024-03-15 15:27:49 +0330

    fix issue

---
+++ index.tsx
@@ -0,0 +1,5 @@
+// src/index.ts
+export { default as Button } from './src/components/Button';
+export { default as ErrorPage } from './app/ErrorPage';
+export { default as NotFound } from './app/NotFound';
+
